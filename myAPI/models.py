9f2fafdde1c86d151729bcfb47145b57e9e588e4
Hannah <hannahpatricebacera@gmail.com>
2022-04-08 10:05:11 +0800

    Created a new model called vehicls

--- myAPI/models.py
+++ myAPI/models.py
@@ -1,3 +1,11 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+class vehicls(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=100)
+
+    def _str_(self):
+        return self.name
